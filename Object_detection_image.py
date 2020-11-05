a438f50c9c29bc7b46e31fd6d77b08fed3a10d7a
NOMAN AHMED <72426747+codinglover-Noman@users.noreply.github.com>
2020-11-05 12:29:58 +0800

    Update Object_detection_image.py

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -1,6 +1,4 @@
 ######## Image Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
 # Date: 1/15/18
 # Description: 
 # This program uses a TensorFlow-trained classifier to perform object detection.
